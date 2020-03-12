f5a9bea4d6dfde92e492ff3b3f01a0e2dd31d611
Jeferson Gonçalves de Freitas <freitas000jeferson@gmail.com>
2020-03-12 09:44:20 -0300

    chat-socket

---
+++ Testes-2-Chat-WebSocket/src/main/java/com/example/testes/api/Testes2ChatWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.testes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testes2ChatWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testes2ChatWebSocketApplication.class, args);
+	}
+
+}
---
+++ Testes-2-Chat-WebSocket/src/test/java/com/example/testes/api/Testes2ChatWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testes2ChatWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
